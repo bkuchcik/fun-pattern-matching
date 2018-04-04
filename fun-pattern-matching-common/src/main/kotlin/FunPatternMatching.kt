4aa9e38f49cfc6cfa8fc8c15572540202c9abce8
Benjamin KUCHCIK <bkuchcik.coursjee@gmail.com>
2018-04-04 07:44:54 +0200

    Some when examples with smart cast

--- fun-pattern-matching-common/src/main/kotlin/FunPatternMatching.kt
+++ fun-pattern-matching-common/src/main/kotlin/FunPatternMatching.kt
@@ -1,3 +1,11 @@
 package com.pattern.matching
 
 class FunPatternMatching
+
+//val customer1ID = CustomerID("Sukyoung")  // Sukyoung--23098234908
+//customer1ID match {
+//    case CustomerID(name) => println(name)  // prints Sukyoung
+//    case _ => println("Could not extract a CustomerID")
+//}
+
+fun <T : Any> T.isSameAs(other: T, vararg properties: T.() -> Any): Boolean = properties.all { it() == other.it() }
--- fun-pattern-matching-common/src/test/kotlin/FunPatternMatchingTest.kt
+++ fun-pattern-matching-common/src/test/kotlin/FunPatternMatchingTest.kt
@@ -8,6 +8,61 @@ class FunPatternMatchingTest {
 
     @Test
     fun should_match_expected_match_result() {
-        asserter.assertEquals("not maching expected", "ok", "ko")
+        val p = Point(5, 5)
+        p.run {
+            when {
+                x == p.x && distance() > 10 -> println("I love $x")
+                else -> println(p)
+            }
+        }
+    }
+
+    @Test
+    fun should_match_expected_match_result_twice() {
+        val p = ALotOfProperty("s", false, 10L, 5, 42.0)
+        p.run {
+            when {
+            //ALotOfProperty("x", true, 52, _, _)
+                s == "x" && b && l == 52L -> println("I love $s")
+                else -> println(p)
+            }
+        }
+    }
+
+    @Test
+    fun usage_of_when_with_apply() {
+        val notification: Notification = Email("sender", "title", "body")
+        notification.apply {
+            when {
+                this is Email && sender.contains("xxxx") -> println("Email from $sender")
+                this is SMS -> println("SMS from $caller")
+                else -> println(this)
+            }
+        }
+    }
+
+    @Test
+    fun usage_of_simple_when() {
+        val notification: Notification = Email("sender", "title", "body")
+        when (notification) {
+            is Email -> notification.apply { println("Email from $sender") }
+            is SMS -> println("SMS from ${notification.caller}")
+            else -> println(this)
+        }
     }
 }
+
+data class Point(val x: Int, val y: Int) {
+    fun distance() = 0
+}
+
+data class ALotOfProperty(val s: String, val b: Boolean, val l: Long, val i: Int, val d: Double)
+
+abstract interface Notification
+
+data class Email(val sender: String, val title: String, val body: String) : Notification
+
+data class SMS(val caller: String, val message: String) : Notification
+
+data class VoiceRecording(val contactName: String, val link: String) : Notification
+
